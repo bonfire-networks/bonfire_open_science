467564458f6772d343de3f43173b58d6c5194f5a
Mayel de Borniol <git@mayel.space>
2025-05-03 20:07:43 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1239

--- lib/integration.ex
+++ lib/integration.ex
@@ -1,7 +1,7 @@
 defmodule Bonfire.OpenScience do
   @moduledoc "./README.md" |> File.stream!() |> Enum.drop(1) |> Enum.join()
 
-  alias Bonfire.Common.Config
+  use Bonfire.Common.Config
   alias Bonfire.Common.Utils
   use Bonfire.Common.E
   import Untangle
