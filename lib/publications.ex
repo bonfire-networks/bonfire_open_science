418114d78a7cd95e8416c25beac78a92ed904261
ivan <ivanminutillo@outlook.it>
2025-08-22 14:39:43 +0200

    https://github.com/bonfire-networks/bonfire-app/issues/1474

--- lib/publications.ex
+++ lib/publications.ex
@@ -8,14 +8,22 @@ defmodule Bonfire.OpenScience.Publications do
   alias Bonfire.OpenScience.OpenAlex.Client
   alias Bonfire.OpenScience.ORCID
   alias Bonfire.OpenScience
+  alias Bonfire.Common.Cache
+  alias Bonfire.Common.Settings
+
+  @default_cache_ttl 1_000 * 60 * 60 * 3  # 3 hours
 
   @doc """
-  Gets author information from OpenAlex for a user.
+  Gets author information from OpenAlex for a user with caching.
   """
   def get_author_info(user) do
-    with {:ok, orcid_id} <- ORCID.user_orcid_id(user),
-         {:ok, author_data} <- Client.fetch_author(orcid_id) do
-      {:ok, author_data}
+    with {:ok, orcid_id} <- ORCID.user_orcid_id(user) do
+      Cache.maybe_apply_cached(
+        &Client.fetch_author/1,
+        [orcid_id],
+        cache_key: "openalex_author:#{orcid_id}",
+        expire: @default_cache_ttl
+      )
     else
       {:error, :no_orcid} -> {:error, "No ORCID ID found for user"}
       error -> error
@@ -23,40 +31,50 @@ defmodule Bonfire.OpenScience.Publications do
   end
 
   @doc """
-  Gets the most recent publication for a user.
+  Gets the most recent publication for a user with caching.
   """
   def get_recent_publication(user) do
-    with {:ok, orcid_id} <- ORCID.user_orcid_id(user),
-         {:ok, publication} <- Client.fetch_recent_publication(orcid_id) do
-      {:ok, publication}
+    with {:ok, orcid_id} <- ORCID.user_orcid_id(user) do
+      Cache.maybe_apply_cached(
+        &Client.fetch_recent_publication/1,
+        [orcid_id],
+        cache_key: "openalex_recent:#{orcid_id}",
+        expire: @default_cache_ttl
+      )
     else
       {:error, :no_orcid} -> {:error, "No ORCID ID found for user"}
-      {:error, :no_publications_found} -> {:error, "No publications found"}
       error -> error
     end
   end
 
   @doc """
-  Gets the most cited publication for a user.
+  Gets the most cited publication for a user with caching.
   """
   def get_most_cited_publication(user) do
-    with {:ok, orcid_id} <- ORCID.user_orcid_id(user),
-         {:ok, publication} <- Client.fetch_most_cited_publication(orcid_id) do
-      {:ok, publication}
+    with {:ok, orcid_id} <- ORCID.user_orcid_id(user) do
+      Cache.maybe_apply_cached(
+        &Client.fetch_most_cited_publication/1,
+        [orcid_id],
+        cache_key: "openalex_cited:#{orcid_id}",
+        expire: @default_cache_ttl
+      )
     else
       {:error, :no_orcid} -> {:error, "No ORCID ID found for user"}
-      {:error, :no_publications_found} -> {:error, "No publications found"}
       error -> error
     end
   end
 
   @doc """
-  Gets publication types distribution for a user.
+  Gets publication types distribution for a user with caching.
   """
   def get_publication_types(user) do
-    with {:ok, orcid_id} <- ORCID.user_orcid_id(user),
-         {:ok, works_by_type} <- Client.fetch_works_by_type(orcid_id) do
-      {:ok, works_by_type}
+    with {:ok, orcid_id} <- ORCID.user_orcid_id(user) do
+      Cache.maybe_apply_cached(
+        &Client.fetch_works_by_type/1,
+        [orcid_id],
+        cache_key: "openalex_types:#{orcid_id}",
+        expire: @default_cache_ttl
+      )
     else
       {:error, :no_orcid} -> {:error, "No ORCID ID found for user"}
       error -> error
@@ -64,37 +82,47 @@ defmodule Bonfire.OpenScience.Publications do
   end
 
   @doc """
-  Gets all publication data for a user in one call.
+  Gets all publication data for a user in one call with caching.
   More efficient than making individual requests.
   """
   def get_all_publication_data(user) do
     with {:ok, orcid_id} <- ORCID.user_orcid_id(user) do
-      # Fetch basic data concurrently
-      %{author_data: author_data, works_by_type: works_by_type} =
-        Client.fetch_complete_data(orcid_id)
-
-      # Fetch publications concurrently
-      tasks = [
-        Task.async(fn -> {:recent_publication, Client.fetch_recent_publication(orcid_id)} end),
-        Task.async(fn ->
-          {:most_cited_publication, Client.fetch_most_cited_publication(orcid_id)}
-        end)
-      ]
-
-      publication_results = Task.await_many(tasks, 15_000)
-
-      {:ok,
-       %{
-         author_data: author_data,
-         works_by_type: works_by_type || [],
-         recent_publication: get_publication_result(publication_results, :recent_publication),
-         most_cited_publication:
-           get_publication_result(publication_results, :most_cited_publication)
-       }}
+      Cache.maybe_apply_cached(
+        &fetch_complete_data_uncached/1,
+        [orcid_id],
+        cache_key: "openalex_complete:#{orcid_id}",
+        expire: @default_cache_ttl
+      )
     else
       {:error, :no_orcid} ->
         {:error, "No ORCID ID found for user"}
     end
+  end
+
+  # Private function that actually fetches all data
+  defp fetch_complete_data_uncached(orcid_id) do
+    # Fetch basic data concurrently
+    %{author_data: author_data, works_by_type: works_by_type} =
+      Client.fetch_complete_data(orcid_id)
+
+    # Fetch publications concurrently  
+    tasks = [
+      Task.async(fn -> {:recent_publication, Client.fetch_recent_publication(orcid_id)} end),
+      Task.async(fn ->
+        {:most_cited_publication, Client.fetch_most_cited_publication(orcid_id)}
+      end)
+    ]
+
+    publication_results = Task.await_many(tasks, 15_000)
+
+    {:ok,
+     %{
+       author_data: author_data,
+       works_by_type: works_by_type || [],
+       recent_publication: get_publication_result(publication_results, :recent_publication),
+       most_cited_publication:
+         get_publication_result(publication_results, :most_cited_publication)
+     }}
   rescue
     error ->
       error(error, "Failed to fetch publication data")
@@ -105,40 +133,55 @@ defmodule Bonfire.OpenScience.Publications do
   Checks which OpenAlex widgets are enabled for a user.
   """
   def enabled_widgets(user) do
-    current_user = user
-
     %{
       recent_publication:
         Settings.get(
           [Bonfire.OpenScience, :recent_publication_widget],
           false,
-          current_user: current_user
+          current_user: user
         ) == true,
       most_cited_publication:
         Settings.get(
           [Bonfire.OpenScience, :most_cited_publication_widget],
           false,
-          current_user: current_user
+          current_user: user
         ) == true,
       author_info:
         Settings.get(
           [Bonfire.OpenScience, :author_info_widget],
           false,
-          current_user: current_user
+          current_user: user
         ) == true,
       author_topics:
         Settings.get(
           [Bonfire.OpenScience, :author_topics_widget],
           false,
-          current_user: current_user
+          current_user: user
         ) == true,
       publication_types:
         Settings.get(
           [Bonfire.OpenScience, :publication_types_widget],
           false,
-          current_user: current_user
+          current_user: user
         ) == true
     }
+  end
+
+  @doc """
+  Invalidates cached OpenAlex data for a user.
+  """
+  def invalidate_user_cache(user) do
+    case ORCID.user_orcid_id(user) do
+      {:ok, orcid_id} -> 
+        Cache.reset(&Client.fetch_author/1, [orcid_id])
+        Cache.reset(&Client.fetch_recent_publication/1, [orcid_id])
+        Cache.reset(&Client.fetch_most_cited_publication/1, [orcid_id])
+        Cache.reset(&Client.fetch_works_by_type/1, [orcid_id])
+        Cache.reset(&fetch_complete_data_uncached/1, [orcid_id])
+        :ok
+      _ -> 
+        :ok
+    end
   end
 
   # Private helpers
